277f342b9e43a79093cfb3168922c8819bba4fa3
Johan Brandhorst <johan@cognitivelogic.com>
2017-03-12 16:31:24 +0000

    Remove generated js files

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 //go:generate gopherjs build -m client.go -o html/index.js
 //go:generate go-bindata -pkg compiled -nometadata -o compiled/client.go -prefix html ./html
+//go:generate bash -c "rm html/*.js*"
 
 import (
 	"github.com/gopherjs/gopherjs/js"
